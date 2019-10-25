46a1d3fad3da75374d714ff477fa158339e2252f
Vlad Gheorghe <31849727+vlad-gheorghe@users.noreply.github.com>
2019-10-25 15:04:05 +0300

    Add files via upload

--- WiFi_Network.h
+++ WiFi_Network.h
@@ -1,7 +1,10 @@
 #ifndef WIFI_NETWORK_H
 #define WIFI_NETWORK_H
 
-const char* ssid = "ssid";  // insert your own ssid
-const char* password = "password";  // and password
+//const char* ssid = "ssid";  // insert your own ssid
+//const char* password = "yourpwd";  // and password
 
 #endif /* WIFI_NETWORK_H */
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
